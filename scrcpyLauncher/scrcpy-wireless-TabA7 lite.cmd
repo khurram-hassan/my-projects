59cd1255bb5383be610f4245ce2cc557843b5d07
Khurram Hassan <khurram.khu@gmail.com>
2023-12-10 14:07:28 +0530

    Adding new device

---
+++ scrcpyLauncher/scrcpy-wireless-TabA7 lite.cmd
@@ -0,0 +1,19 @@
+@REM Before using this script you need to execute following command while phone connected to PC via USB:
+@REM adb tcpip 5555
+
+@set "IP=192.168.29.30"
+adb connect %IP%:5555 | find "connected to"
+@ECHO OFF
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO An error was found. Device could not connect.
+    GOTO eof
+)
+
+@ECHO ON
+scrcpy --turn-screen-off --stay-awake --show-touches --serial=%IP%:5555 --shortcut-mod=lalt,ralt --video-bit-rate=2M --max-fps=15 --push-target=/storage/emulated/0/Download
+adb disconnect %IP%:5555
+
+:eof
+@ECHO OFF
+ECHO Exitting.....
+PAUSE
