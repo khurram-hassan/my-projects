f13de44d239a07a133a6e559c5c479f4626ccae7
Khurram Hassan <khurram.khu@gmail.com>
2024-04-18 18:29:27 +0530

    modify output lines while connecting to my devices

--- scrcpyLauncher/scrcpy-wireless-TabA8.cmd
+++ scrcpyLauncher/scrcpy-wireless-TabA8.cmd
@@ -1,19 +1,23 @@
 @REM Before using this script you need to execute following command while phone connected to PC via USB:
 @REM adb tcpip 5555
 
-@set "IP=192.168.29.29"
-adb connect %IP%:5555 | find "connected to"
 @ECHO OFF
+set "IP=192.168.29.29"
+ECHO adb connect %IP%:5555
+adb connect %IP%:5555 | find "connected to"
+
 IF %ERRORLEVEL% NEQ 0 (
     ECHO An error was found. Device could not connect.
     GOTO eof
 )
+@ECHO:&ECHO:
 
 @ECHO ON
 scrcpy --turn-screen-off --stay-awake --show-touches --serial=%IP%:5555 --shortcut-mod=lalt,ralt --video-bit-rate=2M --max-fps=15 --push-target=/storage/emulated/0/Download
+@ECHO:&ECHO:
 adb disconnect %IP%:5555
 
 :eof
-@ECHO OFF
-ECHO Exitting.....
-PAUSE
+@ECHO:&ECHO:&ECHO:
+@ECHO Exitting.....
+@PAUSE
